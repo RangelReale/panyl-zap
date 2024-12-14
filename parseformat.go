78558822e1b54803997663edbcea7a9568d5f8a0
Rangel Reale <rangelspam@gmail.com>
2024-12-14 07:35:48 -0300

    context

--- parseformat.go
+++ parseformat.go
@@ -24,52 +24,52 @@ var (
 
 func (c ZapJSON) ParseFormat(result *panyl.Process) (bool, error) {
 	// only if json
-	if result.Metadata.StringValue(panyl.Metadata_Structure) == panyl.MetadataStructure_JSON {
+	if result.Metadata.StringValue(panyl.MetadataStructure) == panyl.MetadataStructureJSON {
 		if (c.EncoderConfig.MessageKey == "" || result.Data.HasValue(c.EncoderConfig.MessageKey)) &&
 			(c.EncoderConfig.LevelKey == "" || result.Data.HasValue(c.EncoderConfig.LevelKey)) &&
 			(c.EncoderConfig.TimeKey == "" || result.Data.HasValue(c.EncoderConfig.TimeKey)) {
 
-			result.Metadata[panyl.Metadata_Format] = ZapJSONFormat
+			result.Metadata[panyl.MetadataFormat] = ZapJSONFormat
 
 			if c.EncoderConfig.MessageKey != "" && result.Data.HasValue(c.EncoderConfig.MessageKey) {
-				result.Metadata[panyl.Metadata_Message] = result.Data.StringValue(c.EncoderConfig.MessageKey)
+				result.Metadata[panyl.MetadataMessage] = result.Data.StringValue(c.EncoderConfig.MessageKey)
 			}
 
 			if c.EncoderConfig.NameKey != "" && result.Data.HasValue(c.EncoderConfig.NameKey) {
-				result.Metadata[panyl.Metadata_Category] = result.Data.StringValue(c.EncoderConfig.NameKey)
+				result.Metadata[panyl.MetadataCategory] = result.Data.StringValue(c.EncoderConfig.NameKey)
 			} else if c.EncoderConfig.CallerKey != "" && result.Data.HasValue(c.EncoderConfig.CallerKey) {
-				result.Metadata[panyl.Metadata_Category] = result.Data.StringValue(c.EncoderConfig.CallerKey)
+				result.Metadata[panyl.MetadataCategory] = result.Data.StringValue(c.EncoderConfig.CallerKey)
 			}
 
 			if c.EncoderConfig.TimeKey != "" && result.Data.HasValue(c.EncoderConfig.TimeKey) {
 				ts, err := time.Parse(zapTimestampFormat, result.Data.StringValue(c.EncoderConfig.TimeKey))
 				if err == nil {
-					result.Metadata[panyl.Metadata_Timestamp] = ts
+					result.Metadata[panyl.MetadataTimestamp] = ts
 				}
 			}
 
 			if c.EncoderConfig.LevelKey != "" && result.Data.HasValue(c.EncoderConfig.LevelKey) {
 				switch result.Data.StringValue(c.EncoderConfig.LevelKey) {
 				case "fatal", "FATAL", "panic", "PANIC", "dpanic", "DPANIC":
-					result.Metadata[panyl.Metadata_Level] = panyl.MetadataLevel_FATAL
+					result.Metadata[panyl.MetadataLevel] = panyl.MetadataLevel_FATAL
 				case "error", "ERROR":
-					result.Metadata[panyl.Metadata_Level] = panyl.MetadataLevel_ERROR
+					result.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelERROR
 				case "warn", "WARN":
-					result.Metadata[panyl.Metadata_Level] = panyl.MetadataLevel_WARNING
+					result.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelWARNING
 				case "info", "INFO":
-					result.Metadata[panyl.Metadata_Level] = panyl.MetadataLevel_INFO
+					result.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelINFO
 				case "debug", "DEBUG":
-					result.Metadata[panyl.Metadata_Level] = panyl.MetadataLevel_DEBUG
+					result.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelDEBUG
 				}
 			}
 
 			// if level is error/fatal, and have an "error" key, show it as the error message
-			if result.Metadata.HasValue(panyl.Metadata_Message) &&
-				result.Metadata.HasValue(panyl.Metadata_Level) &&
-				(result.Metadata[panyl.Metadata_Level] == panyl.MetadataLevel_ERROR || result.Metadata[panyl.Metadata_Level] == panyl.MetadataLevel_FATAL) &&
+			if result.Metadata.HasValue(panyl.MetadataMessage) &&
+				result.Metadata.HasValue(panyl.MetadataLevel) &&
+				(result.Metadata[panyl.MetadataLevel] == panyl.MetadataLevelERROR || result.Metadata[panyl.MetadataLevel] == panyl.MetadataLevel_FATAL) &&
 				result.Data.HasValue("error") {
-				result.Metadata[panyl.Metadata_Message] = fmt.Sprintf("%s [error: %s]",
-					result.Metadata[panyl.Metadata_Message], result.Data.StringValue("error"))
+				result.Metadata[panyl.MetadataMessage] = fmt.Sprintf("%s [error: %s]",
+					result.Metadata[panyl.MetadataMessage], result.Data.StringValue("error"))
 			}
 
 			return true, nil

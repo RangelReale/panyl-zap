3a476c99f45aa01c0ae381273158422ac66f6b92
Rangel Reale <rangelspam@gmail.com>
2024-12-15 08:41:07 -0300

    rename Process to Item

--- parseformat.go
+++ parseformat.go
@@ -23,52 +23,52 @@ var (
 	zapTimestampFormat = "2006-01-02T15:04:05.000Z07:00"
 )
 
-func (c ZapJSON) ParseFormat(ctx context.Context, result *panyl.Item) (bool, error) {
+func (c ZapJSON) ParseFormat(ctx context.Context, item *panyl.Item) (bool, error) {
 	// only if json
-	if result.Metadata.StringValue(panyl.MetadataStructure) == panyl.MetadataStructureJSON {
-		if (c.EncoderConfig.MessageKey == "" || result.Data.HasValue(c.EncoderConfig.MessageKey)) &&
-			(c.EncoderConfig.LevelKey == "" || result.Data.HasValue(c.EncoderConfig.LevelKey)) &&
-			(c.EncoderConfig.TimeKey == "" || result.Data.HasValue(c.EncoderConfig.TimeKey)) {
+	if item.Metadata.StringValue(panyl.MetadataStructure) == panyl.MetadataStructureJSON {
+		if (c.EncoderConfig.MessageKey == "" || item.Data.HasValue(c.EncoderConfig.MessageKey)) &&
+			(c.EncoderConfig.LevelKey == "" || item.Data.HasValue(c.EncoderConfig.LevelKey)) &&
+			(c.EncoderConfig.TimeKey == "" || item.Data.HasValue(c.EncoderConfig.TimeKey)) {
 
-			result.Metadata[panyl.MetadataFormat] = ZapJSONFormat
+			item.Metadata[panyl.MetadataFormat] = ZapJSONFormat
 
-			if c.EncoderConfig.MessageKey != "" && result.Data.HasValue(c.EncoderConfig.MessageKey) {
-				result.Metadata[panyl.MetadataMessage] = result.Data.StringValue(c.EncoderConfig.MessageKey)
+			if c.EncoderConfig.MessageKey != "" && item.Data.HasValue(c.EncoderConfig.MessageKey) {
+				item.Metadata[panyl.MetadataMessage] = item.Data.StringValue(c.EncoderConfig.MessageKey)
 			}
 
-			if c.EncoderConfig.NameKey != "" && result.Data.HasValue(c.EncoderConfig.NameKey) {
-				result.Metadata[panyl.MetadataCategory] = result.Data.StringValue(c.EncoderConfig.NameKey)
-			} else if c.EncoderConfig.CallerKey != "" && result.Data.HasValue(c.EncoderConfig.CallerKey) {
-				result.Metadata[panyl.MetadataCategory] = result.Data.StringValue(c.EncoderConfig.CallerKey)
+			if c.EncoderConfig.NameKey != "" && item.Data.HasValue(c.EncoderConfig.NameKey) {
+				item.Metadata[panyl.MetadataCategory] = item.Data.StringValue(c.EncoderConfig.NameKey)
+			} else if c.EncoderConfig.CallerKey != "" && item.Data.HasValue(c.EncoderConfig.CallerKey) {
+				item.Metadata[panyl.MetadataCategory] = item.Data.StringValue(c.EncoderConfig.CallerKey)
 			}
 
-			if c.EncoderConfig.TimeKey != "" && result.Data.HasValue(c.EncoderConfig.TimeKey) {
-				ts, err := time.Parse(zapTimestampFormat, result.Data.StringValue(c.EncoderConfig.TimeKey))
+			if c.EncoderConfig.TimeKey != "" && item.Data.HasValue(c.EncoderConfig.TimeKey) {
+				ts, err := time.Parse(zapTimestampFormat, item.Data.StringValue(c.EncoderConfig.TimeKey))
 				if err == nil {
-					result.Metadata[panyl.MetadataTimestamp] = ts
+					item.Metadata[panyl.MetadataTimestamp] = ts
 				}
 			}
 
-			if c.EncoderConfig.LevelKey != "" && result.Data.HasValue(c.EncoderConfig.LevelKey) {
-				switch result.Data.StringValue(c.EncoderConfig.LevelKey) {
+			if c.EncoderConfig.LevelKey != "" && item.Data.HasValue(c.EncoderConfig.LevelKey) {
+				switch item.Data.StringValue(c.EncoderConfig.LevelKey) {
 				case "error", "ERROR", "fatal", "FATAL", "panic", "PANIC", "dpanic", "DPANIC":
-					result.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelERROR
+					item.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelERROR
 				case "warn", "WARN":
-					result.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelWARNING
+					item.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelWARNING
 				case "info", "INFO":
-					result.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelINFO
+					item.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelINFO
 				case "debug", "DEBUG":
-					result.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelDEBUG
+					item.Metadata[panyl.MetadataLevel] = panyl.MetadataLevelDEBUG
 				}
 			}
 
 			// if level is error, and have an "error" key, show it as the error message
-			if result.Metadata.HasValue(panyl.MetadataMessage) &&
-				result.Metadata.HasValue(panyl.MetadataLevel) &&
-				(result.Metadata[panyl.MetadataLevel] == panyl.MetadataLevelERROR) &&
-				result.Data.HasValue("error") {
-				result.Metadata[panyl.MetadataMessage] = fmt.Sprintf("%s [error: %s]",
-					result.Metadata[panyl.MetadataMessage], result.Data.StringValue("error"))
+			if item.Metadata.HasValue(panyl.MetadataMessage) &&
+				item.Metadata.HasValue(panyl.MetadataLevel) &&
+				(item.Metadata[panyl.MetadataLevel] == panyl.MetadataLevelERROR) &&
+				item.Data.HasValue("error") {
+				item.Metadata[panyl.MetadataMessage] = fmt.Sprintf("%s [error: %s]",
+					item.Metadata[panyl.MetadataMessage], item.Data.StringValue("error"))
 			}
 
 			return true, nil
